95beaee3844f2fa1a789e61f5724cc3d10653f44
Thierry Chau <thierry.chau@gmail.com>
2023-07-25 18:57:30 +0000

    added flexible_from.erb

--- app.rb
+++ app.rb
@@ -9,20 +9,20 @@ parsed_exchangerate = JSON.parse(raw_exchangerate)
 
 symbols_hash = parsed_exchangerate.fetch("symbols")
 
-@list_currency = []
+list_currency = []
 
 symbols_hash.each do |currency_symbol, currency_hash|
-  @list_currency.push(currency_symbol)
+  list_currency.push(currency_symbol)
 end
 
 get("/") do
-
+  @list_currency = list_currency
+  
   erb(:currency_pairs)
 end
 
-@list_currency.each do |currency|
-  get("/#{currency}") do
-    
-    erb(:flexible)
-  end
+get("/:user_currency") do
+  @list_currency = list_currency
+
+  erb(:flexible_from)
 end
