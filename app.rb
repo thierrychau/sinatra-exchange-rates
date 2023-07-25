e56d2b551995709d37faa7f22757948276e88444
Thierry Chau <thierry.chau@gmail.com>
2023-07-25 15:21:28 +0000

    moving currency list to root

--- app.rb
+++ app.rb
@@ -3,18 +3,26 @@ require "sinatra/reloader"
 require "http"
 require "json"
 
+exchangerate_url = "https://api.exchangerate.host/symbols"
+raw_exchangerate = HTTP.get(exchangerate_url)
+parsed_exchangerate = JSON.parse(raw_exchangerate)
+
+symbols_hash = parsed_exchangerate.fetch("symbols")
+
+@list_currency = []
+
+symbols_hash.each do |currency_symbol, currency_hash|
+  @list_currency.push(currency_symbol)
+end
+
 get("/") do
-  exchangerate_url = "https://api.exchangerate.host/symbols"
-  raw_exchangerate = HTTP.get(exchangerate_url)
-  parsed_exchangerate = JSON.parse(raw_exchangerate)
-
-  symbols_hash = parsed_exchangerate.fetch("symbols")
-
-  @list_currency = []
-
-  symbols_hash.each do |currency_symbol, currency_hash|
-    @list_currency.push(currency_symbol)
-  end
 
   erb(:currency_pairs)
+end
+
+@list_currency.each do |currency|
+  get("/#{currency}") do
+    
+    erb(:flexible)
+  end
 end
